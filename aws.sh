af58107d32171376ae45dcb36d8e200076ad0e9f
liaozd <liao_zd@hotmail.com>
2015-02-22 20:42:36 +0800

    first website commit

--- aws.sh
+++ aws.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-ssh -vvv -i ../keys/myubuntu.pem ubuntu@52.74.10.37
+#ssh -vvv -i ../keys/myubuntu.pem ubuntu@52.74.10.37
+ssh -vvv -i ../keys/myubuntu.pem ubuntu@52.74.57.11
 
