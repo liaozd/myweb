a418b7a7295e263ea3d07c36e537e917508e6d68
liaozd <liao_zd@hotmail.com>
2015-02-22 23:20:48 +0800

    url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,10 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-<<<<<<< HEAD
-
-=======
     url(r'^$', 'myblog.views.index', name='index'),
->>>>>>> b76d8059e6a5a04b1e93194a31498be1c67ac792
     url(r'^admin/', include(admin.site.urls)),
 )
