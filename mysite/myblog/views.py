02fc7c8b6fa39b1b0d628a7af4c9becd0c73b7ce
liaozd <liao_zd@hotmail.com>
2015-02-23 20:49:02 +0800

    test for drone.io

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('This page shows a list of most recent posts.')
+    return HttpResponse('This page shows a list of most recent posts. H')
