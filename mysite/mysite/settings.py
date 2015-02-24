8939192290b6936adf769e8afc5d208517b8ef05
liaozd <liao_zd@hotmail.com>
2015-02-24 23:42:16 +0800

    fix 'tuple' object has no attribute 'startswith'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,7 +84,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static'), )
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = STATIC_ROOT
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
