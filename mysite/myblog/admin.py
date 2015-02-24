e7e34d91b5476539be293bb22995b76cf87ba51e
liaozd <liao_zd@hotmail.com>
2015-02-24 15:41:25 +0800

    markdown works

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from . import models
+from django.db.models import TextField
 from django_markdown.admin import MarkdownModelAdmin
+from django_markdown.widgets import AdminMarkdownWidget
 
 
-# class EntryAdmin(admin.ModelAdmin):
 class EntryAdmin(MarkdownModelAdmin):
     list_display = ('title', 'created')
     prepopulated_fields = {'slug': ('title',)}
+    # http://arunrocks.com/recreating-the-building-a-blog-in-django-screencast/
+    # python2.x should add this
+    formfield_overrides = {TextField: {'widget': AdminMarkdownWidget}}
 
 # this is interesting, sync slug with title, when inputting in title
 # by javascript
-# http://arunrocks.com/recreating-the-building-a-blog-in-django-screencast/
 admin.site.register(models.Entry, EntryAdmin)
