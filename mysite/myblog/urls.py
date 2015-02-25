f2d8df4268a8fb17505b0008a3368785cef52d3f
liaozd <liao_zd@hotmail.com>
2015-02-25 15:38:49 +0800

    change urls

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = patterns('',
     url(r'^$', views.BlogIndex.as_view(), name='index'),
+    url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry_detail"),
 )
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,5 +6,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include('django_markdown.urls')),
     url(r'^', include('myblog.urls')),
-    url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(),)
 )
