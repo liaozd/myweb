c0ae8e682de5d85ca291c76baf081d3c33a7d285
liaozd <liao_zd@hotmail.com>
2015-02-25 08:12:36 +0800

    add nginx static settings

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -6,3 +6,8 @@ class BlogIndex(generic.ListView):
     queryset = models.Entry.objects.published()
     template_name = 'home.html'
     paginate_by = 2
+
+
+class BlogDetail(generic.DetailView):
+    model = models.Entry
+    template_name = 'post.html'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from myblog import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include('django_markdown.urls')),
     url(r'^', include('myblog.urls')),
+    url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(),)
 )
