5f3a8aa058db7f1d36fd2808f40b43a503ffba30
liaozd <liao_zd@hotmail.com>
2015-02-25 14:48:10 +0800

    add nginx static settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,7 +85,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = STATIC_ROOT
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
