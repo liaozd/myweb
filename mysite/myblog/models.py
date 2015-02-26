38dcbc43cc3a70b8b7f21f4bd2a888393d6f16e8
liaozd <liao_zd@hotmail.com>
2015-02-26 21:45:01 +0800

    add tag

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,4 +1,12 @@
+from django.core.urlresolvers import reverse
 from django.db import models
+
+
+class Tag(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.slug
 
 
 class EntryQuerySet(models.QuerySet):
@@ -13,11 +21,16 @@ class Entry(models.Model):
     publish = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag)
 
     objects = EntryQuerySet.as_manager()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        # seems to be not used in django
+        return reverse("entry_detail", kwargs={"slug": self.slug})
 
     class Meta:
         verbose_name = 'Blog Entry'
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -63,7 +63,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        }
 }
 
 # Internationalization
@@ -84,7 +84,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
