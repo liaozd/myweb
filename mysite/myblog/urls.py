a52cbda69a5fe7a66e7f846759d09b6e4a0e4128
liaozd <liao_zd@hotmail.com>
2015-02-28 22:59:40 +0800

    no STATIC_ROOT settings

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -5,3 +5,4 @@ urlpatterns = patterns('',
                        url(r'^$', views.BlogIndex.as_view(), name='index'),
                        url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(), name='entry_detail'),
                        )
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from mysite import settings
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^markdown/', include('django_markdown.urls')),
                        url(r'^', include('myblog.urls')),
                        )
+
