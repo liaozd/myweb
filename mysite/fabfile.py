69356aa863cead0e0e69c8c889acd1c7b5748b48
liaozd <liao_zd@hotmail.com>
2015-03-01 17:22:14 +0800

    change layout

--- mysite/fabfile.py
+++ mysite/fabfile.py
@@ -7,8 +7,9 @@ env.roledefs['static'] = ['ubuntu@52.74.57.11', ]
 # Where the static files get collected locally. Your STATIC_ROOT setting.
 env.local_static_root = STATIC_ROOT
 
-env.remote_static_root = '/home/ubuntu/git-repos/myweb/mysite/static/static_root'
-
+# will create a sub dir under th
+# env.remote_static_root = '/home/ubuntu/git-repos/myweb/mysite/static/static_root'
+env.remote_static_root = '/home/ubuntu/git-repos/myweb/mysite/static'
 @roles('static')
 def deploy_static():
     local(BASE_DIR + '/manage.py collectstatic --noinput -v3 --clear')
