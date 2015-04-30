036f1ec0de39a2db2f738a731706a3dd9ad32132
liaozd <liao_zd@hotmail.com>
2015-04-30 17:06:51 +0800

    circle deploy test

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,28 @@
 from fabric.api import *
 from fabric.contrib import project
-from mysite.settings import STATIC_ROOT, BASE_DIR
+# from mysite.settings import STATIC_ROOT, BASE_DIR
 
-env.roledefs['static'] = ['ubuntu@52.74.40.130', ]
-# Where the static files get collected locally. Your STATIC_ROOT setting.
-env.local_static_root = STATIC_ROOT
+# use ~/.ssh/config
+env.use_ssh_config = True
 
-env.remote_static_root = '/home/ubuntu/git-repos/myweb/mysite/static'
-@roles('static')
-def deploy_static():
-    local(BASE_DIR + '/manage.py collectstatic --noinput -v3 --clear')
-    project.rsync_project(
-        remote_dir=env.remote_static_root,
-        local_dir=env.local_static_root,
-        # delete=True # <-- Be careful with this
-    )
+env.roledefs = {
+    'myweb': ['ec2-52-74-40-130.ap-southeast-1.compute.amazonaws.com']
+}
 
+
+# # Where the static files get collected locally. Your STATIC_ROOT setting.
+# env.local_static_root = STATIC_ROOT
+#
+# env.remote_static_root = '/home/ubuntu/git-repos/myweb/mysite/static'
+# @roles('aws_myweb')
+# def deploy_static():
+#     local(BASE_DIR + '/manage.py collectstatic --noinput -v3 --clear')
+#     project.rsync_project(
+#         remote_dir=env.remote_static_root,
+#         local_dir=env.local_static_root,
+#         # delete=True # <-- Be careful with this
+#     )
+
+@roles('myweb')
+def log():
+    run('tail -n 150 /var/log/docker/beijing_station.log')
