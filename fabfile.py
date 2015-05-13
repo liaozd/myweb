63b6762b60f345dd8a59fd1510bcc143e9ad35de
liaozd <liao_zd@hotmail.com>
2015-05-13 09:52:09 +0800

    export docker db container env

--- fabfile.py
+++ fabfile.py
@@ -49,7 +49,7 @@ def deploy(to='local', branch='staging'):
         'IMAGES_NONE=$(docker images -q --filter "dangling=True"); \
          [ -z "$IMAGES_NONE"  ] || docker rmi -f $IMAGES_NONE',
         # TODO consider backup/restore your data
-        '[ -z "$DB_CONTAINER"  ] || DB_CONTAINER=$(docker run -e "POSTGRES_PASSWORD=pass" -d --name db postgres)',
+        '[ -z "$DB_CONTAINER"  ] || export DB_CONTAINER=$(docker run -e "POSTGRES_PASSWORD=pass" -d --name db postgres)',
         'docker run -e "POSTGRES_PASSWORD=pass" -d --name db postgres',
         '{0} up -d'.format(docker_exec_prefix),
         # migrate the django database
