3936421d6ce69e5e05dc7348e618b62d4eb92d89
liaozd <liao_zd@hotmail.com>
2015-09-20 22:45:28 +0800

    [wip:docker arch] nginx conf file name problem fix

--- src/conf/urls.py
+++ src/conf/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from src.fgfw import views
+
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^markdown/', include('django_markdown.urls')),
                        url(r'^', include('myblog.urls')),
-                       url(r'^fgfw/$', views.fgfw, name='fgfw'),
                        )
 
