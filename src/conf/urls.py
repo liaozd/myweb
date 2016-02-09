f8bf6684763d1cfcd87ab223e7395b930c8c9e79
liaozd <liao_zd@hotmail.com>
2016-02-09 20:44:50 +0800

    add taoist url

--- src/conf/urls.py
+++ src/conf/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include('django_markdown.urls')),
     url(r'^', include('myblog.urls')),
+    url(r'^taoist/', include('taoist.urls'), name="taoist"),
 ]
-
