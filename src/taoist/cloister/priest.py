715d33410d0ae913af4c3257047838a996f3686f
liaozd <liao_zd@hotmail.com>
2016-02-13 00:51:20 +0800

    add download/upload files

---
+++ src/taoist/cloister/priest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+class Priest(object):
+    """docstring for ClassName"""
+    def listen(self):
+        pass
+
+    def download(self):
+        pass
+
+    def update(self):
+        pass
+
+
---
+++ src/taoist/cloister/taoist.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+class Taoist(object):
+    """docstring for ClassName"""
+
+    def push(self):
+        pass
+
+    def upload(self):
+        pass
